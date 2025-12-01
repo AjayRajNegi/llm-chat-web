07a8a49104676c233f01890c8e287c62ec6cdaea
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-01 17:27:33 +0530

    Fixed vercel errors

--- app/model/Output.tsx
+++
@@ -1,8 +0,0 @@
-const response = await ollama.chat({
-  model: "llama3.1",
-  messages: [{ role: "user", content: "Why is the sky blue?" }],
-});
-console.log(response.message.content);
-export function Output() {
-  return <>Output</>;
-}
